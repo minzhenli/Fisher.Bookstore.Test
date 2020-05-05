d2b539ebae72997e08485ac19e5ad1b02dfebd3a
minzhenli <min.205@osu.edu>
2020-05-04 21:39:03 -0400

    hin#

---
+++ BookTests.cs
@@ -0,0 +1,49 @@
+
+using System;
+using Xunit;
+using Fisher.Bookstore.Models;
+
+namespace Fisher.Bookstore.Test
+{
+    public class BookTests
+    {
+        [Fact]
+        public void AddBookPrice()
+        {
+            //Arrange
+            var book = new Book()
+            {
+                Id = 1,
+                Title = "My Test Book",
+                Price = 9.99
+            };
+
+            //Act
+            book.Price = 7.99;
+
+            //Assert
+            Assert.Equal(7.99, book.Price);
+        }
+
+        [Fact]
+        public void TestMemberDiscount()
+        {
+            //Arrange
+            var book = new Book()
+            {
+                Id = 1,
+                Title = "My Test Book",
+                Price = 9.99
+            };
+
+            //Act
+            book.ApplyMemberDiscount();
+
+            //Assert
+            var discountPrice = 9.99 * .9;
+            Assert.Equal(discountPrice, book.Price);
+        }
+    }
+}
+
+
